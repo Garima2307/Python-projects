3f98e61233937a8902de8f50a5f44c398d7efe6a
Garima2307 <ggupta230705@gmail.com>
2026-07-06 00:11:30 +0530

    Hangman project along with hanman_words and hangman_art fikes added in Day 7

---
+++ Day6_escapingTheMaze.py
@@ -0,0 +1,26 @@
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+    
+# while front_is_clear():
+#     move()
+# turn_left()
+# while not at_goal():
+#     if right_is_clear():
+#         turn_right()
+#         move()
+#     elif front_is_clear():
+#         move()
+#     else:
+#         turn_left()
+    
+
+        
+# c:\Users\LENOVO\OneDrive\Pictures\Screenshots\Screenshot 2026-07-03 125633.png
+
+            
+        
+        
+    
+    
---
+++ Playground/playground.py
@@ -0,0 +1,1 @@
+#Experimenting codes here ......NO PROJECTS INSIDE THIS FOLDER
